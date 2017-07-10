39c694ff5f338e72f46ddcf01512e39c851ce28b
Tristan Wolf <wolftristan@hotmail.com>
2017-07-10 11:07:35 -0400

    users can now sign up and log in to gain authorization to write to the Firebase database

---
+++ ImageRecognitionApp/SignUpViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SignUpViewController.swift
+//  ImageRecognitionApp
+//
+//  Created by Tristan Wolf on 2017-07-10.
+//  Copyright © 2017 Tristan Wolf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
+class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func signUpAction(_ sender: UIButton) {
+        
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "You must enter a username and password", preferredStyle: .alert)
+            
+            let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(alertAction)
+            
+            present(alertController, animated: true, completion: nil)
+        } else {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!){(user, error) in
+                
+                if self.passwordTextField.text == self.confirmPasswordTextField.text {
+                    if error == nil {
+                        print("Signup was successful")
+                        //self.navigationController?.popViewController(animated: true)
+
+                    } else {
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: "The passwords do not match", preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    
+}
